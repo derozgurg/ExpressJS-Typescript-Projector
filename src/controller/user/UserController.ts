15efb09d69f92839d80057bd7c5e2ceffc34b127
derozgur <derozgur@gmail.com>
2019-05-15 15:59:11 +0300

    some comments

--- src/controller/user/UserController.ts
+++ src/controller/user/UserController.ts
@@ -3,11 +3,19 @@ import ApiControllerBase, { ApiController, HttpAction } from '../../core/control
 import authenticatedMiddleware from '../../policy/authenticatedMiddleware';
 import authorizationMiddleware from '../../policy/authorizationMiddleware';
 
+
+/**
+ * If you don't specify any path for this controller, it uses the controller name automatically for path
+ * for this controller the path will be '/user'
+ */
 @ApiController({
     authenticatedMiddleware,
     authorizationMiddleware
+    //path:'/user'
 })
 export default class UserController extends ApiControllerBase {
+
+    //http://localhost:3001/user/userid
     @HttpAction({
         method: 'get',
         path: '/:id'})
@@ -15,6 +23,8 @@ export default class UserController extends ApiControllerBase {
         response.send('get user by id ');
     }
 
+
+    //http://localhost:3001/user
     @HttpAction({
         method: 'get',
         path: '/',
@@ -25,6 +35,8 @@ export default class UserController extends ApiControllerBase {
         response.send('Users List');
     }
 
+
+    //http://localhost:3001/user/userid/posts
     @HttpAction({
         method: 'get',
         path:'/:id/posts',
@@ -32,10 +44,14 @@ export default class UserController extends ApiControllerBase {
         roles:['super','admin']
     })
     public userPosts(request: express.Request, response: express.Response) {
-        //http://localhost:3001/user/TestUserName/posts
+
         response.send(`User posts for ${request.params.id}`);
     }
 
+    /**
+     * If you don't specify any path and action name is equals method, it automatically creates controller base path as a method
+     * path will be '/user' for this action
+     */
     @HttpAction({method: 'post'})
     public post(request: express.Request, response: express.Response) {
         console.log('Create user', this.path)
